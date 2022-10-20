ee2b7a95756ee7d380d6b4123ed5c7034081c9e7
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-21 00:03:05 +0500

    SSR started

--- sanity-md-build/pages/index.tsx
+++ sanity-md-build/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
 
 const Home: NextPage = () => {
   return (
@@ -29,3 +30,27 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts
+    }
+  }
+
+}
